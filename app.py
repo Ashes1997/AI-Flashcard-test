9510370de30494107595f5d973b6c3442b6b869f
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-09 23:22:28 +0000

    Got desired print of content, need to ensure reply is consistent format

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ flashcard = "SQL queries using select, where, group by and having clauses utilis
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a tutor that creates flashcards for your students in the following format. Question:.......... /n/n Answer:........... , The questions and answers are based off the submitted text and should be no longer than 50 words each. You rely heavily on the given text and rely little on external information. Reply with only one question, one correct answer, and 3 incorrect answers with false details."},
+    {"role": "system", "content": "You are a tutor that creates flashcards for your students in the following format. Question:.......... \n\n Answer:........... , The questions and answers are based off the submitted text and should be no longer than 50 words each. You rely heavily on the given text and rely little on external information. Reply with only one question, one correct answer, and 3 incorrect answers with false details."},
     {"role": "user", "content": flashcard}
   ]
 )
 
-#print(completion.choices[0].message)
-print(completion.choices[0])
+print(completion.choices[0].message.content)
