42b6105dacec0d3031bbc9061ce988944e6acf1f
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-09 23:20:04 +0000

    created desired chat functionality

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import os
 from openai import OpenAI
 
 client = OpenAI()
-
+flashcard = "SQL queries using select, where, group by and having clauses utilise the where clause first, then group by, then select, then having"
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    {"role": "system", "content": "You are a tutor that creates flashcards for your students in the following format. Question:.......... /n/n Answer:........... , The questions and answers are based off the submitted text and should be no longer than 50 words each. You rely heavily on the given text and rely little on external information. Reply with only one question, one correct answer, and 3 incorrect answers with false details."},
+    {"role": "user", "content": flashcard}
   ]
 )
 
-print(completion.choices[0].message)
+#print(completion.choices[0].message)
+print(completion.choices[0])
