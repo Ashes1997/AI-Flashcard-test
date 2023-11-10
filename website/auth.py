d726d26c60f1c7eb7a85cc2d15fd6ac407ae088e
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-10 16:22:43 +0000

    Added html for login and signup
    Need to finish post and get functionality

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
-  return "<p>Login</p>"
+  return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-  return "<p>Logout</p>"
+  return render_template("logout.html") #going to change need to redirect
+  
 
 @auth.route('/sign-up')
 def sign_up():
-  return "<p>Sign up</p>"
+  return render_template("sign-up.html")
 
