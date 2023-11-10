7f79b580c2f7fa948674ea19a966f772fb8ba723
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-10 13:50:23 +0000

    Added views to website
    Home, login, logout and sign- up pages

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,11 @@ import os
 def create_app():
   app = Flask(__name__)
   app.config['SECRET KEY'] = 'blahblahblah'
+
+  from .views import views
+  from .auth import auth
+
+  app.register_blueprint(views, url_prefix='/')
+  app.register_blueprint(auth, url_prefix='/')
+  
   return app
