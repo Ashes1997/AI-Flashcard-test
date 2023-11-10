bbeb78cf180f161ae67e7417a1162eabcd7a8b22
Ashes1997 <149199767+Ashes1997@users.noreply.github.com>
2023-11-10 11:51:22 +0000

    Completed chatGPT functionality
    Created prompt method for creating flashcard and moved into separate file to tidy code. Could revisit to allow for increased number of flashcards to generate.

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-import openai
-import os
-from openai import OpenAI
+from chatGPT import flash_create
 
-client = OpenAI()
-flashcard = "SQL queries using select, where, group by and having clauses utilise the where clause first, then group by, then select, then having"
+prompt = "Object inheritance in Java is where an object inherits methods and attributes from it's super class"
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a tutor that creates flashcards for your students in the following format. Question:.......... \n\n Answer:........... , The questions and answers are based off the submitted text and should be no longer than 50 words each. You rely heavily on the given text and rely little on external information. Reply with only one question, one correct answer, and 3 incorrect answers with false details."},
-    {"role": "user", "content": flashcard}
-  ]
-)
-
-print(completion.choices[0].message.content)
+flashcard = flash_create(prompt)
+for f in flashcard:
+  print(f)
